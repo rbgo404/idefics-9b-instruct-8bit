e45f5b28111d321d7e0ac6ce175524f27495d5fe
ujjawalPeak01 <119571874+ujjawalPeak01@users.noreply.github.com>
2023-10-14 08:53:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+import json
+import numpy as np
+import torch
+from transformers import IdeficsForVisionText2Text, AutoProcessor
 
+
+class InferlessPythonModel:
+	def initialize(self):
+		checkpoint = "HuggingFaceM4/idefics-9b-instruct"
+		self.model = IdeficsForVisionText2Text.from_pretrained(checkpoint, load_in_8bit=True, device_map="auto")
+		self.processor = AutoProcessor.from_pretrained(checkpoint)
+
+
+	def infer(self, inputs):
+		prompts = inputs["prompts"]
+
+		inputs = self.processor(prompts, return_tensors="pt").to("cuda")
+		bad_words_ids = self.processor.tokenizer(["<image>", "<fake_token_around_image>"], add_special_tokens=False).input_ids
+		generated_ids = self.model.generate(**inputs, bad_words_ids=bad_words_ids, max_length=500)
+		generated_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)
+
+		final_result = ""
+
+		for i, t in enumerate(generated_text):
+            print(f"{i}:\n{t}\n")
+            final_result += f"{i}:\n{t}\n"
+
+        return {"generated_text": final_result}
+
+
+	def finalize(self):
+		self.model = None
+		self.processor = None
